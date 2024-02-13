32d67c198c8cf3f8513722ca005b34730eddf388
cold <125336084+fukgoin@users.noreply.github.com>
2024-02-13 10:40:39 -0800

    Update views.js

--- api/views.js
+++ api/views.js
@@ -1,14 +1,1 @@
-// pages/api/views.js
 
-let views = 471;
-
-export default (req, res) => {
-    if (req.method === 'POST') {
-      let views = 1;
-      if (localStorage.getItem('views')) {
-        views = parseInt(localStorage.getItem('views')) + 1;
-      }
-      localStorage.setItem('views', views);
-      res.status(200).json({ views });
-    }
-  }  
